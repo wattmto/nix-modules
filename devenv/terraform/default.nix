e6018934d0e70561bbaf6e90434d2aee8fe4ab1a
wattmto <dev@wattmto.dev>
2024-12-29 21:42:35 +0900

    devenvModules.terraform: add package option

--- devenv/terraform/default.nix
+++ devenv/terraform/default.nix
@@ -10,10 +10,20 @@ in
 {
   options.my.terraform = {
     enable = lib.mkEnableOption "tools for Terraform development";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.terraform;
+      defaultText = lib.literalExpression "pkgs.terraform";
+      description = "The Terraform package to use.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    languages.terraform.enable = true;
+    languages.terraform = {
+      enable = true;
+      package = cfg.package;
+    };
 
     packages = [
       pkgs.awscli2
