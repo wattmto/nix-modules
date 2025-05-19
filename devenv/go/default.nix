77c81509e660c8f64756a8509c3ef4f53a24613b
Wataru Matsuoka <93639059+wattmto@users.noreply.github.com>
2025-05-19 13:41:14 +0000

    fixup! devenv: fix buildWithSpecificGo

--- devenv/go/default.nix
+++ devenv/go/default.nix
@@ -7,8 +7,6 @@
 let
   cfg = config.my.go;
 
-  goVersion = (lib.versions.major cfg.package.version) + (lib.versions.minor cfg.package.version);
-
   buildGoModule = pkgs.buildGoModule.override { go = cfg.package; };
   buildWithSpecificGo = pkg: pkg.override { inherit buildGoModule; };
 in
@@ -33,9 +31,7 @@ in
 
     packages = [
       pkgs.gcc
-      # TODO: Build with specific go version after https://github.com/NixOS/nixpkgs/pull/339057 fixes
-      # (buildWithSpecificGo pkgs.golangci-lint)
-      pkgs.golangci-lint
+      (buildWithSpecificGo pkgs.golangci-lint)
       (buildWithSpecificGo pkgs.ko)
     ];
   };
